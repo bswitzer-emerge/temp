460478118d2fd68f7c1780954be578debcfca5ae
Greg Gant <me@greggant.com>
2023-02-27 11:28:40 -0800

    Register for access model

--- assets/register-for-access-model.js
+++ assets/register-for-access-model.js
@@ -8,9 +8,27 @@ console.log(modal, "Test")
 openButton.addEventListener("click", function(event) {
     event.preventDefault();
     modal.classList.add("show");
+    //modal isn't attached to document query as it doesn't exist yet
+    initializeClickHandler();
   });
   
   closeButton.addEventListener("click", function() {
     modal.classList.remove("show");
   });
+
+
+function initializeClickHandler() {
+    const parentDiv = document.querySelector('.register-for-access-modal.show');
+    parentDiv.addEventListener('click', (event) => {
+        // Ignore the click event if it originated from a child element
+        if (event.target !== event.currentTarget) {
+        event.stopPropagation();
+        return;
+        }
+        // Perform the desired action for clicking on the parent div
+        console.log("test");
+        modal.classList.remove("show");
+    });
+}
+
   
