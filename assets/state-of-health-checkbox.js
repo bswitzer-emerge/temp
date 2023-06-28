faf9469b1fb521ca312a57a07cf2bf0e138b400c
Greg Gant <me@greggant.com>
2023-06-27 21:15:45 -0700

    State of health

--- assets/state-of-health-checkbox.js
+++ assets/state-of-health-checkbox.js
@@ -2,56 +2,50 @@
     This script is for the state of health filter 
     on the collections
 
-    onPageLoadSoh(); - A URL grabby script to fill out the form if the page reloads
+    Idea:
+    Make a virtualized form, to simulate checkboxes ranges. Really we're hiding the real form and making an interface for it.
 
+    Problem I had:
+    When shopify’s form is interacted with, it wipes the DOM where the form lives and shits out a new form, this means if you change the form with JS values are lost. That’s been the issue from the get go. Simply listening to form changes meant it’d see the change, make the changes only to be wiped.
 
-    updateInputs() - no argumeents, attached to event listener
+    Solution: 
+    Using the MutationObserver now I can see when the form has been actually changed then run my logic against the form to enable and disable my custom check boxes. 
 
-    ensureRange() - no arguements, makes sure that the low and high
-                    do not conflict with each other (ex low price can't be higher than high price)
+    Logic:
 
-    checkTheBoxes() - looks for the #soh-health-form-items, and then
-                    checks the ones that match the input range. This  is a work around for the lack of filtering avaliable nu default in facets.liquid
-                    It has to simulate a mouse click for the original mystery meat Shopify JS to work.
-    timerCountDown / timerCountReset - using the classic StackOverflow solution
-                    https://stackoverflow.com/questions/4220126/run-javascript-function-when-user-finishes-typing-instead-of-on-key-u
+    Step 1: On page load make sure to disable the inputs if there’s no items within the checkbox ranges
+    Step 2: on the change trigger the immediate disabling (there’s a latency before it refreshes the form, and destroys the changes). This way the user can see the disabled/enabled inputs. This has to work with the all the filters.
+    Step 3: On the mutation (or when the form is reloaded), re-run all that same logic! 
 */
 
-const sohLowInput = document.getElementById('soh-low');
-const sohHighInput = document.getElementById('soh-high');
-const newInputs =  document.querySelectorAll('#state-of-health-checkboxes input[type="checkbox"]');
 
-const checkboxes = document.querySelectorAll('#soh-health-form-items input[type="checkbox"]');
+
 const filteredValues = [];
-
-onPageLoadSoh(); // fire this on first run to make sure the values are reflected.
-
-//timer for keyup
-let typingTimer;                //timer identifier
-const doneTypingInterval = 700;  //time in ms
-
 let lowValue;
 let highValue;
 
-// Iterate over each checkbox and assign an event listener
-newInputs.forEach(function(newInput) {
-    newInput.addEventListener('change', function() {
-    // Check if the checkbox is checked
-    highLowCheck(this.id);
 
-    if (this.checked) {
-      // Checkbox is checked
-      checkTheBoxes(true);
-    } else {
-      // Checkbox is unchecked
-      checkTheBoxes(false);
-      //console.log('Checkbox ' + this.id + ' is unchecked.');
-    }
-  });
-});
+function setSOHlistener() {
+    //console.log("setSOHlistener")
+    // Iterate over each checkbox and assign an event listener
+    const sohCheckboxes =  document.querySelectorAll('#state-of-health-checkboxes input[type="checkbox"]');
+    sohCheckboxes.forEach(function(sohCheckbox) {
+        sohCheckbox.addEventListener('change', function() {
+        // Check if the checkbox is checked
+            highLowCheck(this.id); // assertain the range based on which check box 
+            console.log("this.checked", this.checked);
+            if (this.checked) {
+                checkTheBoxes(true);
+            } else {
+                checkTheBoxes(false);
+            //console.log('Checkbox ' + this.id + ' is unchecked.');
+            }
+        });
+    });
+}
 
 function highLowCheck(id) {
-    console.log(`id: ${id}`);
+    //console.log(`id: ${id}`);
     if (id == "faux-59") {
         lowValue = 0;
         highValue = 59;
@@ -77,9 +71,11 @@ function highLowCheck(id) {
 
 
 // apply the filter to actual code
-function checkTheBoxes(switcher) {
+function checkTheBoxes(checkMe) {
 
     console.log(`checkTheBoxes() lowValue ${lowValue}, highValue ${highValue}`)
+    const checkboxes = document.querySelectorAll('#soh-health-form-items input[type="checkbox"]');
+
     filteredValues.length = 0; // Clear the array
     
     // Go through entire list of checkboxes
@@ -88,34 +84,18 @@ function checkTheBoxes(switcher) {
 
         if (checkboxValue >= lowValue && checkboxValue <= highValue) {
             // Perform the desired action, e.g., check the checkbox
-            if (switcher) {
+            if (checkMe) {
                 checkbox.checked = true;
             } else {
                 checkbox.checked = false;
             }
             filteredValues.push(checkboxValue);
         } else {
-            //checkbox.checked = false;
             filteredValues.push(checkboxValue);
-        }
-        // simulate mouse click at the end of the array to trigger Shopify
-        const isLastItem = index === checkboxes.length - 1;
-        if (isLastItem) {
-            //checkbox.click();
         }
     } 
 }
 
-
-// timer
-function timerCountdown() {
-    timerCountReset();
-    typingTimer = setTimeout(updateInputs, doneTypingInterval);
-}
-
-function timerCountReset() {
-    clearTimeout(typingTimer);
-};
 
 // Page loading!
 function onPageLoadSoh() {
@@ -168,55 +148,143 @@ function onPageLoadSoh() {
 
     // On the page load, this will check if any filters are set and if any inventory sets are missing. If there aren't any batteries that exist within a check box's range, then they will be greyed out.
     if (sohValues.length == 0 ) {
-        console.log("I'm da bess")
-        let test59, test60, test70, test80, test90;
+        
+    }
+    disableEnable();
 
 
-        for (const [index, checkbox] of checkboxes.entries()) {
-            const checkboxValue = parseInt(checkbox.value);
-            doesValueRangeExist(checkboxValue);
+}
 
-        } 
-        function doesValueRangeExist (value){
-            if ( value > 0 && value <= 59) {
-                test59 = true;
-            }
-            if ( value >= 60 && value <= 69) {
-                test60 = true;
-            }
-            if ( value >= 70 && value <= 79) {
-                test70 = true;
-            }
-            if ( value >= 80 && value <= 89) {
-                test80 = true;
-            }
-            if ( value >= 90 && value <= 100) {
-                test90 = true;
-            }
-        }
-        if (!test59) {
-            const checkbox = document.getElementById('faux-59');
-            checkbox.disabled = true;
-        }
-        if (!test60) {
-            const checkbox = document.getElementById('faux-60');
-            checkbox.disabled = true;
-        }
-        if (!test70) {
-            const checkbox = document.getElementById('faux-70');
-            checkbox.disabled = true;
-        }
-        if (!test80) {
-            const checkbox = document.getElementById('faux-80');
-            checkbox.disabled = true;
-        }
+//big ugly mess to clean
+function disableEnable() {
+    let test59, test60, test70, test80, test90;
+    const checkboxes2 = document.querySelectorAll('#soh-health-form-items input[type="checkbox"]');
 
-        if (!test90) {
-            const checkbox = document.getElementById('faux-90');
-            checkbox.disabled = true;
-         }
+    for (const [index, checkbox] of checkboxes2.entries()) {
+        const checkboxValue = parseInt(checkbox.value);
+
+        // console.log("disabled", checkbox.disabled)
+        doesValueRangeExist(checkboxValue, checkbox.disabled);
+
+    } 
+    function doesValueRangeExist (value, disabled){
+        if ( value > 0 && value <= 59 && disabled == false ) {
+            test59 = true;
+        }
+        if ( value >= 60 && value <= 69 && disabled == false ) {
+            test60 = true;
+        }
+        if ( value >= 70 && value <= 79 && disabled == false ) {
+            test70 = true;
+        }
+        if ( value >= 80 && value <= 89 && disabled == false ) {
+            test80 = true;
+        }
+        if ( value >= 90 && value <= 100 && disabled == false ) {
+            test90 = true;
+        }
     }
 
     
+    // Stupid brute force: Just go down the list and check if each variable is false,to disable inputs, then if not enable them.
+    if (!test59) {
+        const checkbox = document.getElementById('faux-59');
+        checkbox.disabled = true;
+    } else {
+        const checkbox = document.getElementById('faux-59');
+        checkbox.disabled = false;
+    }
+
+    if (!test60) {
+        const checkbox = document.getElementById('faux-60');
+        checkbox.disabled = true;
+    } else {
+        const checkbox = document.getElementById('faux-60');
+        checkbox.disabled = false;
+    }
+
+    if (!test70) {
+        const checkbox = document.getElementById('faux-70');
+        checkbox.disabled = true;
+    } else {
+        const checkbox = document.getElementById('faux-60');
+        checkbox.disabled = false;
+    }
+
+    if (!test80) {
+        const checkbox = document.getElementById('faux-80');
+        checkbox.disabled = true;
+    } else {
+        const checkbox = document.getElementById('faux-80');
+        checkbox.disabled = false;
+    }
+    if (!test90) {
+        const checkbox = document.getElementById('faux-90');
+        checkbox.disabled = true;
+        } else {
+        const checkbox = document.getElementById('faux-90');
+        checkbox.disabled = false;
+    }
+    // get the state again
+    const checkboxes3 = document.querySelectorAll('#soh-health-form-items input[type="checkbox"]');
+    let areAnyBoxesChecked = false;
+    for (const [index, checkbox] of checkboxes3.entries()) {
+        if (checkbox.checked == true ) {
+            areAnyBoxesChecked = true;
+        }
+    } 
+    if (areAnyBoxesChecked) {
+        if (detectCheckedCheckboxes(0, 59)) {
+            document.getElementById('faux-59').checked = true;
+        } 
+        if (detectCheckedCheckboxes(60, 69)) {
+            document.getElementById('faux-60').checked = true;
+        } 
+        if (detectCheckedCheckboxes(70, 79)) {
+            document.getElementById('faux-70').checked = true;
+        } 
+        if (detectCheckedCheckboxes(80, 79)) {
+            document.getElementById('faux-80').checked = true;
+        } 
+    }
+    //end brute force
 }
 
+const detectCheckedCheckboxes = (val1, val2 ) => {
+    const checkboxes = Array.from(document.querySelectorAll('#soh-health-form-items input[type="checkbox"]'));
+    let isChecked = false;
+    for (const checkbox of checkboxes) {
+      const value = parseInt(checkbox.value);
+  
+      if (value >= val1 && value <= val2 && checkbox.checked && !checkbox.disabled) {
+        isChecked = true;
+        break;
+      }
+    }
+  
+    return isChecked;
+};
+
+
+
+// The magic of MutationObserver to detect when the dom has refreshed from shopify.
+const observeMe = document.getElementById('FacetsWrapperDesktop');
+// Create a new MutationObserver
+const observer = new MutationObserver(mutationsList => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList' || mutation.type === 'characterData') {
+        console.log('Content changed:');
+        disableEnable();
+        setSOHlistener(); // reset the listener on change
+    }
+  }
+});
+
+// Start observing the div for changes
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+// we will not disconnect! :)
+observer.observe(observeMe, { childList: true, characterData: true, subtree: true });
+
+
+setSOHlistener(); // init the listener, this will be reinited.
+onPageLoadSoh(); // fire this on first run to make sure the values are reflected.
