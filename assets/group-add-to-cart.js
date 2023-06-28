9142f1d4e8b4b80ca2ffcaaecebfe9cecddda3d0
Greg Gant <me@greggant.com>
2023-06-28 14:45:43 -0700

    Add to cart group

--- assets/group-add-to-cart.js
+++ assets/group-add-to-cart.js
@@ -1,41 +1,67 @@
+
 function updateButtonState() {
- 
+    const addToCartButton = document.querySelector('#add_to_cart-heading');
     const checkedCheckboxes = document.querySelectorAll('.addtocart-checkbox input:checked');
     const isChecked = checkedCheckboxes.length > 0;
-    console.log("isChecked: ", isChecked)
+    console.log("isChecked: ", isChecked, "isChecked:", isChecked, "addToCartButton:", addToCartButton);
 
     if (isChecked) {
-      addToCartButton.classList.remove('disabled');
+        addToCartButton.classList.remove('disabled');
     } else {
-      addToCartButton.classList.add('disabled');
+        addToCartButton.classList.add('disabled');
+    }
+}
+
+function addToCartListener() {
+    document.getElementById("add_to_cart-heading").addEventListener("click", () => {
+        // Find all checked checkboxes
+        const checkboxes = document.querySelectorAll('.card-product-list__col0 input[type="checkbox"]:checked');
+        // Store the values of checked checkboxes
+        const values = Array.from(checkboxes).map(checkbox => checkbox.value);
+        console.log("values: ", values)
+    
+        // Click buttons inside divs matching checkbox values
+        values.forEach(value => {
+            const div = document.getElementById(value);
+            if (div) {
+            const button = div.querySelector('button');
+            if (button) {
+                console.log("hit")
+                button.click();
+            }
+            }
+        });
+    });
+}
+
+function queryForAddToCart() {
+    const addToCartCheckboxes = document.querySelectorAll('.addtocart-checkbox');
+    addToCartCheckboxes.forEach((checkbox) => {
+        checkbox.addEventListener('change', updateButtonState);
+    });
+}
+
+addToCartListener();
+queryForAddToCart();
+
+
+
+  // The magic of MutationObserver to detect when the dom has refreshed from shopify.
+const observeInventoryList = document.getElementById('ProductGridContainer');
+// Create a new MutationObserver
+const observerForList = new MutationObserver(mutationsList => {
+  for (let mutation of mutationsList) {
+
+    if (mutation.type === 'childList' || mutation.type === 'characterData' || mutation.type === "attributes") {
+        console.log('product-grid changed');
+        queryForAddToCart();
+        addToCartListener();
+        
     }
   }
+});
 
-  const addToCartCheckboxes = document.querySelectorAll('.addtocart-checkbox');
-  const addToCartButton = document.querySelector('#add_to_cart-heading');
-
-  addToCartCheckboxes.forEach((checkbox) => {
-    checkbox.addEventListener('change', updateButtonState);
-  });
-
-
-  document.getElementById("add_to_cart-heading").addEventListener("click", () => {
-    // Find all checked checkboxes
-    const checkboxes = document.querySelectorAll('.card-product-list__col0 input[type="checkbox"]:checked');
-    // Store the values of checked checkboxes
-    const values = Array.from(checkboxes).map(checkbox => checkbox.value);
-
-    console.log("values: ", values)
-
-    // Click buttons inside divs matching checkbox values
-    values.forEach(value => {
-      const div = document.getElementById(value);
-      if (div) {
-        const button = div.querySelector('button');
-        if (button) {
-          console.log("hit")
-          button.click();
-        }
-      }
-    });
-  });
+// Start observing the div for changes
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+// we will not disconnect! :)
+observerForList.observe(observeInventoryList, { childList: true, characterData: true, subtree: true });
