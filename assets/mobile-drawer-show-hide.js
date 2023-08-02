dfdb348608163f449ca3454ce2baddffb3bd8855
Greg Gant <me@greggant.com>
2023-08-02 15:47:50 -0700

    CRMK-255 CRMK-256 - Mobile: Filters need to match desktop

---
+++ assets/mobile-drawer-show-hide.js
@@ -0,0 +1,16 @@
+const heading = document.querySelector('.facets__heading');
+const filterDrawer = document.querySelector('.filter-drawer');
+const filterDrawerBib = document.querySelector('.filter-drawer-bib');
+const filterDrawerClose = document.querySelector('.filter-drawer-close');
+
+
+// Function to toggle the "open" class on the filter drawer
+const toggleFilterDrawer = () => {
+  filterDrawer.classList.toggle('open');
+  filterDrawerBib.classList.toggle('open')
+};
+
+// Add event listener to the heading to trigger the toggle function
+heading.addEventListener('click', toggleFilterDrawer);
+filterDrawerBib.addEventListener('click', toggleFilterDrawer);
+filterDrawerClose.addEventListener('click', toggleFilterDrawer);
